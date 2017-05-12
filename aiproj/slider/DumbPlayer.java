99d768bc83f00ada609037c2d8ba556875a4df1f
AlinaTan <sut2@student.unimelb.edu.au>
2017-05-12 17:32:29 +1000

    Added Part B

---
+++ aiproj/slider/DumbPlayer.java
@@ -0,0 +1,21 @@
+package aiproj.slider;
+
+public class DumbPlayer implements SliderPlayer {
+
+	@Override
+	public void init(int dimension, String board, char player) {
+
+	}
+
+	@Override
+	public void update(Move move) {
+		
+
+	}
+
+	@Override
+	public Move move() {
+		return null;
+	}
+
+}
